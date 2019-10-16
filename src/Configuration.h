2b509f33511abe1f88346b8d1f91607c333e469a
Rob McKenzie <TheZeroBeast@me.com>
2019-10-17 08:28:11 +1000

    SE HT Thermistor table fix applied.

--- src/Configuration.h
+++ src/Configuration.h
@@ -16,9 +16,10 @@ extern uint16_t nPrinterType;
 extern PGM_P sPrinterName;
 
 // Firmware version
-#define FW_VERSION "TZB3.0.1"
-// BASED ON PRUSA Stock FW 3.8.1-RC1
-#define FW_COMMIT_NR   2468
+#define FW_VERSION "TZB3.0.2"
+// BASED ON PRUSA Stock FW 3.8.1-RC1 & LA1.5 from Vertigo235
+// https://github.com/vertigo235/Build-Prusa-LA-15
+#define FW_COMMIT_NR   2469
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_RC

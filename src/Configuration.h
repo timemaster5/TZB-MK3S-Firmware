9a8b5ac9a5b56f0e803757d2f17c324cf834fec3
Rob McKenzie <TheZeroBeast@me.com>
2021-01-19 21:25:47 +1000

    BN Synced.

--- src/Configuration.h
+++ src/Configuration.h
@@ -19,7 +19,7 @@ extern PGM_P sPrinterName;
 #define FW_VERSION "TZB3.2.0"
 // BASED ON PRUSA Stock FW 3.9.3
 // https://github.com/vertigo235/Build-Prusa-LA-15
-#define FW_COMMIT_NR   2489
+#define FW_COMMIT_NR   2491
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_RC

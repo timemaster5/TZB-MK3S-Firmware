6041555e20576855b471a403397f8076472ebe64
Rob <thezerobeast@icloud.com>
2021-09-21 10:31:20 +1000

    Aligning BN and Ver for release 3.3.0

--- src/Configuration.h
+++ src/Configuration.h
@@ -16,10 +16,9 @@ extern uint16_t nPrinterType;
 extern PGM_P sPrinterName;
 
 // Firmware version
-#define FW_VERSION "TZB3.2.1"
+#define FW_VERSION "TZB3.3.0"
 // BASED ON PRUSA Stock FW 3.10.0
-// https://github.com/vertigo235/Build-Prusa-LA-15
-#define FW_COMMIT_NR   2512
+#define FW_COMMIT_NR   2516
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_RC
